30d76098e9ebd7a34fd0e69ac3892b920704c64f
Wang Jun <wangjun@istartech.cn>
2018-07-08 18:29:38 +0800

    update routes

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -4,38 +4,33 @@
 *********/
 const index = async (ctx) => {
     ctx.body = {
-      message: 'Hello World!!!'
-    }
-  }
-  
+      message: "Hello World!!!"
+    };
+  };
   const create = async (ctx) => {
     ctx.body = {
-      message: 'Hello World!!!'
-    }
-  }
-  
+      message: "Hello World!!!"
+    };
+  };
   const show = async (ctx) => {
     ctx.body = {
-      message: 'Hello World!!!'
-    }
-  }
-  
+      message: "Hello World!!!"
+    };
+  };
   const update = async (ctx) => {
     ctx.body = {
-      message: 'Hello World!!!'
-    }
-  }
-  
+      message: "Hello World!!!"
+    };
+  };
   const destroy = async (ctx) => {
     ctx.body = {
-      message: 'Hello World!!!'
-    }
-  }
-  
+      message: "Hello World!!!"
+    };
+  };
   export {
     index,
     create,
     show,
     update,
     destroy
-  }
+  };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import * as userController from "../controller/user";
 const api = KoaRouter();
 
 api.get("/users", userController.index);
+api.post("/users", userController.create);
 api.get("/users/:id", userController.show);
-
+api.put("/users/:id", userController.update);
+api.delete("/uesrs/:id", userController.destroy);
 export default api;
