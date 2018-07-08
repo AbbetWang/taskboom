8ae7ef12a3653c481cbb1e987bbe7919091a7dfe
Wang Jun <wangjun@istartech.cn>
2018-07-08 18:26:23 +0800

    add common

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import KoaRouter from 'koa-router'
-import * as userController from '../controller/user'
-const api = KoaRouter()
+import KoaRouter from "koa-router";
+import * as userController from "../controller/user";
+const api = KoaRouter();
 
-api.get('/users', userController.index)
-api.get('/users/:id', userController.show)
+api.get("/users", userController.index);
+api.get("/users/:id", userController.show);
 
-export default api
+export default api;
